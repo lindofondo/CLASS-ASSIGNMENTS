854f3bb197db098c3ec40581389832e67d0e644d
lindofondo <lindofondo.a@gmail.com>
2017-02-14 18:05:24 -0500

    Add files via upload
    Greeting.

---
+++ Greeting.py
@@ -0,0 +1,10 @@
+def main():
+    a= str(input("Hey, what's your name? "))
+    print ("Nice to meet you,",a)
+
+    b= str(input("So, what's your favorite color? "))
+    print ("That's a nice color!")
+
+    c= str(input("What's your favorite food? "))
+    print ("Sounds yummy!")
+main()
